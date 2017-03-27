8580ca4fd78a893ddaa6d38117bce5498f1cbd8d
Mky135 <Mky.Flores@hotmail.com>
2017-03-27 09:37:50 -0500

    adds backup for the gear autos

--- src/com/team2502/robot2017/command/autonomous/GearAutoLeft.java
+++ src/com/team2502/robot2017/command/autonomous/GearAutoLeft.java
@@ -8,6 +8,7 @@ public class GearAutoLeft extends CommandGroup {
 	public GearAutoLeft() 
 	{
 		addSequential(new EncDriveDistanceCommand()); // fine tune this and try to use encoders if possible
+//		addSequential(new DriveTimeCommand(1D));
 //		addSequential(new WaitCommand(1));
 //		addSequential(new NavXMoveCommand(60, 4));
 //		addSequential(new AutoVCommand(2));
