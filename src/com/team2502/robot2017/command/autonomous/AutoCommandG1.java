124830a2a9ff92c59066b6b48ff3a9338f4b0687
64008508 <64008508@hs-05230-macair.ad.edenpr.org>
2017-04-02 21:10:23 -0500

    Fixed Formatting & Removed Deprecated PushGear Command

--- src/com/team2502/robot2017/command/autonomous/AutoCommandG1.java
+++ src/com/team2502/robot2017/command/autonomous/AutoCommandG1.java
@@ -1,50 +1,16 @@
 package com.team2502.robot2017.command.autonomous;
 
-<<<<<<< HEAD
-import com.team2502.robot2017.command.DriveTimeCommand;
 import com.team2502.robot2017.command.FlywheelCommand;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/Develop
-=======
-import com.team2502.robot2017.command.autonomous.*;
-
->>>>>>> origin/Develop
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class AutoCommandG1 extends CommandGroup
 {
-    /**
-     * For testing 
-     */
     public AutoCommandG1()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         addSequential(new EncDriveDistanceCommand());
         addSequential(new WaitCommand(1D));
-<<<<<<< HEAD
-   
         addSequential(new WaitCommand(1D));
-
-=======
-        addSequential(new PushGearCommand(true));
-        addSequential(new WaitCommand(1D));
-        addSequential(new PushGearCommand(false));
->>>>>>> origin/Develop
-        addSequential(new DriveTimeCommand(1D));
-        
-=======
-
-        addSequential(new DriveTimeCommand(1.35D, .7));//1.5
-        addSequential(new WaitCommand(1D));
->>>>>>> origin/Develop
-
-        
-=======
-        addSequential(new EncDriveDistanceCommand());       
->>>>>>> origin/Develop
+        addSequential(new DriveTimeCommand(1D, 1D));
     }
 }
