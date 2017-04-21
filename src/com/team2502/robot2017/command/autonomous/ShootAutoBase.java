44d5d3fcd1b89da65a9028d113aad1cbec6f0526
Mky135 <Mky.Flores@hotmail.com>
2017-04-21 08:56:50 -0500

    Added a better way to check strings

--- src/com/team2502/robot2017/command/autonomous/ShootAutoBase.java
+++ src/com/team2502/robot2017/command/autonomous/ShootAutoBase.java
@@ -12,7 +12,10 @@ public class ShootAutoBase extends CommandGroup {
 	 * @param allianceColor The color of the alliance("red" or "blue"). If it is not either "red" or "blue", the robot will turn 180.
 	 */
 	private double angle = -60;
-	
+	static String red = "red";
+	static String blue = "blue";
+	static String redU = red.toUpperCase();
+	static String blueU = blue.toUpperCase();
 	public ShootAutoBase(String allianceColor)
 	{
 		// shoot balls into boiler
@@ -25,9 +28,9 @@ public class ShootAutoBase extends CommandGroup {
 	    
 	    // Pull away from boiler
 	    addSequential(new DriveTimeCommand(.5/3, -1));
-	    
-	    if(allianceColor.toLowerCase() == "red") { addSequential(new NavXMoveCommand(-angle, 1.5)); }
-	    else if(allianceColor.toLowerCase() == "blue") { addSequential(new NavXMoveCommand(angle, 1.5)); }
+	    String allianceColorU = allianceColor.toUpperCase();
+	    if(allianceColorU.hashCode() == redU.hashCode()) { addSequential(new NavXMoveCommand(-angle, 1.5)); }
+	    else if(allianceColorU.hashCode() == blueU.hashCode()) { addSequential(new NavXMoveCommand(angle, 1.5)); }
 	    else { addSequential(new NavXMoveCommand(180, 10)); }
 	}
 }
