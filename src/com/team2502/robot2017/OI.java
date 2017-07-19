24b9c454c2f8b770e891d45e53f2259a33f6a29d
Ritik Mishra <ritik.mishra314@gmail.com>
2017-07-18 23:08:52 -0500

    Comments, style, docs

--- src/com/team2502/robot2017/OI.java
+++ src/com/team2502/robot2017/OI.java
@@ -14,50 +14,50 @@ public final class OI
     public static final Joystick JOYSTICK_DRIVE_LEFT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_LEFT);
     public static final Joystick JOYSTICK_DRIVE_RIGHT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_RIGHT);
     public static final Joystick JOYSTICK_FUNCTION = new Joystick(RobotMap.Joystick.JOYSTICK_FUNCTION);
-    
+
     private static final Button SWITCH_DRIVE_TRANSMISSION = new JoystickButton(JOYSTICK_DRIVE_RIGHT, RobotMap.Joystick.Button.SWITCH_DRIVE_TRANSMISSION);
 
-	private static final Button RESET_ENC_POSITION = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.RESET_ENC_POS);
+    private static final Button RESET_ENC_POSITION = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.RESET_ENC_POS);
 
-	private static final Button ADD_AGITATOR_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_AGITATOR_SPEED);
-	private static final Button SUB_AGITATOR_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_AGITATOR_SPEED);
+    private static final Button ADD_AGITATOR_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_AGITATOR_SPEED);
+    private static final Button SUB_AGITATOR_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_AGITATOR_SPEED);
 
-	private static final Button ADD_COLSON_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_COLSON_SPEED);
-	private static final Button SUB_COLSON_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_COLSON_SPEED);
+    private static final Button ADD_COLSON_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_COLSON_SPEED);
+    private static final Button SUB_COLSON_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_COLSON_SPEED);
 
-	private static final Button ADD_BANEBOT_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_BANEBOT_SPEED);
-	private static final Button SUB_BANEBOT_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_BANEBOT_SPEED);
+    private static final Button ADD_BANEBOT_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.ADD_BANEBOT_SPEED);
+    private static final Button SUB_BANEBOT_SPEED = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.SUB_BANEBOT_SPEED);
 
-	private static final Button ADD_FLYWHEEL_SPEED = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.ADD_FLYWHEEL_SPEED);
-	private static final Button SUB_FLYWHEEL_SPEED = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.SUB_FLYWHEEL_SPEED);
+    private static final Button ADD_FLYWHEEL_SPEED = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.ADD_FLYWHEEL_SPEED);
+    private static final Button SUB_FLYWHEEL_SPEED = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.SUB_FLYWHEEL_SPEED);
 
 //	private static final Button VISION_ALIGN = new JoystickButton(JOYSTICK_DRIVE_LEFT, RobotMap.Joystick.Button.VISION_ALIGN);
 
-	private static final Button CLIMBER = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.CLIMBER);
-    
+    private static final Button CLIMBER = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.CLIMBER);
+
 
     static
     {
-        SWITCH_DRIVE_TRANSMISSION.whenPressed(new SwitchDriveTransmissionCommand());
+//        SWITCH_DRIVE_TRANSMISSION.whenPressed(new SwitchDriveTransmissionCommand());
+        // Above is obsolete due to automatic transmission
 
-		RESET_ENC_POSITION.whenPressed(new ResetEncodersCommand()); 
-		
+        RESET_ENC_POSITION.whenPressed(new ResetEncodersCommand());
+
         ADD_AGITATOR_SPEED.whenPressed(new ChangeSpeedAgitatorCommand(true));
         SUB_AGITATOR_SPEED.whenPressed(new ChangeSpeedAgitatorCommand(false));
-        
+
         ADD_COLSON_SPEED.whenPressed(new ChangeSpeedColsonCommand(true));
         SUB_COLSON_SPEED.whenPressed(new ChangeSpeedColsonCommand(false));
-        
+
         ADD_BANEBOT_SPEED.whenPressed(new ChangeSpeedBanebotCommand(true));
         SUB_BANEBOT_SPEED.whenPressed(new ChangeSpeedBanebotCommand(false));
-        
+
         ADD_FLYWHEEL_SPEED.whenPressed(new ChangeSpeedFlywheelCommand(true));
         SUB_FLYWHEEL_SPEED.whenPressed(new ChangeSpeedFlywheelCommand(false));
-        
+
 //        VISION_ALIGN.whileHeld(new TeleopVisionCommand());
 
 
-        
     }
 
     public static void init() {}
--- src/com/team2502/robot2017/command/teleop/DriveCommand.java
+++ src/com/team2502/robot2017/command/teleop/DriveCommand.java
@@ -9,16 +9,22 @@ import com.team2502.robot2017.subsystem.DriveTrainTransmissionSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * Control the drivetrain during teleop.
+ * Takes care of all Drivetrain related operations during Teleop, including automatic shifting
+ * Automatic shifting will:
+ * <li>
+ *     <ul>Space out shifting by at least 1/2 second</ul>
+ *     <ul>Invert itself if the driver holds a special button</ul>
+ *     <ul>Only shift when going mostly straight</ul>
+ *     <ul>Shift up if accelerating, going fast, and the driver is pushing hard on the sticks</ul>
+ *     <ul>Shift down if the sticks are being pushed but there is no acceleration</ul>
+ *     <ul>Shift down if the sticks aren't being pushed hard and the robot is going slow</ul>
+ * </li>
  */
 public class DriveCommand extends Command
 {
     private final DriveTrainSubsystem driveTrainSubsystem;
     private final DriveTrainTransmissionSubsystem transmission;
     private final AHRS navx;
-    private double accel;
-    private double turning;
-    private double speed;
 
 
     public DriveCommand()
@@ -36,55 +42,69 @@ public class DriveCommand extends Command
         driveTrainSubsystem.setTeleopSettings();
     }
 
-    boolean shiftedUp = false;
+    private boolean shiftedUp = false;
 
     @Override
-    protected void execute() {
+    protected void execute()
+    {
         driveTrainSubsystem.drive();
 
 
         // Check that at least 1/2 second has passed since last shifting
-        if((System.currentTimeMillis()-Robot.SHIFTED) >= 500)
+        if((System.currentTimeMillis() - Robot.SHIFTED) >= 500)
         {
+
+            // Do the opposite if the driver is forcing a shift
             if(OI.JOYSTICK_DRIVE_RIGHT.getRawButton(RobotMap.Joystick.Button.SWITCH_DRIVE_TRANSMISSION))
             {
                 System.out.println("Shifting " + (shiftedUp ? "down" : "up") + " forced by driver.");
                 transmission.setGear(!shiftedUp);
             }
-            // Make sure the driver isn't forcing anything
+
+            // If the driver is cool with auto shifting doing its thing
             else
             {
-                if ( Math.abs(navx.getRawAccelY()) > 0.15 && driveTrainSubsystem.avgVel() > RobotMap.Motor.SHIFT_UP_THRESHOLD && OI.joysThreshold(0.8, true))
+                // Make sure that we're going mostly straight
+                if(driveTrainSubsystem.turningFactor() < 0.1)
                 {
-                    if(!shiftedUp)
-                    {
-                        shiftedUp = true;
-                        System.out.println("Shifting up.");
-                    }
-                    transmission.setGear(true);
+                    double accel = Math.abs(navx.getRawAccelY());
+                    double speed = driveTrainSubsystem.avgVel();
 
-                } else if (Math.abs(navx.getRawAccelY()) <= 0.1 && OI.joysThreshold(0.8, false))
-                {
-                    if(shiftedUp)
+                    // Shift up if we are accelerating and going fast and the driver is putting the joystick at least 80% forward or backward
+                    if(accel > 0.15 && speed > RobotMap.Motor.SHIFT_UP_THRESHOLD && OI.joysThreshold(0.8, true))
                     {
-                        shiftedUp = false;
-                        System.out.println("Shifting down.");
-                    }
-                    transmission.setGear(false);
+                        if(!shiftedUp)
+                        {
+                            shiftedUp = true;
+                            System.out.println("Shifting up.");
+                        }
+                        transmission.setGear(true);
 
-                } else if (OI.joysThreshold(30, false) && driveTrainSubsystem.avgVel() < RobotMap.Motor.SHIFT_DOWN_THRESHOLD)
-                {
-                    if(shiftedUp)
-                    {
-                        shiftedUp = false;
-                        System.out.println("Shifting down0.");
                     }
-                    transmission.setGear(false);
+
+                    // If we are not accelerating very fast but the driver is still pushing forward we shift down because it is probably a pushy push
+                    else if(accel <= 0.1 && OI.joysThreshold(0.8, false))
+                    {
+                        if(shiftedUp)
+                        {
+                            shiftedUp = false;
+                            System.out.println("Shifting down.");
+                        }
+                        transmission.setGear(false);
+
+                    }
+
+                    // If we're going slow and the driver wants it to be that way we shift down
+                    else if(OI.joysThreshold(30, false) && speed < RobotMap.Motor.SHIFT_DOWN_THRESHOLD)
+                    {
+                        if(shiftedUp)
+                        {
+                            shiftedUp = false;
+                            System.out.println("Shifting down.");
+                        }
+                        transmission.setGear(false);
+                    }
                 }
-            }
-            // Make sure that we're not turning super hard
-            if(driveTrainSubsystem.turningFactor() < 0.1)
-            {
             }
 
         }
