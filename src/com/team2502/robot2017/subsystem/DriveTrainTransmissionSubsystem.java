ad4733d0cb5e9460f7378349e0afa73f05b24dfa
Ritik Mishra <ritik.mishra314@gmail.com>
2017-07-20 18:15:59 -0500

    Math, Change to navx shift case
    Some math stuff has been added to DriveTrainTransmissionSubsystem.java
    
    In the middle shift case, instead of low acceleration, we wait for deceleration instead
    "

--- src/com/team2502/robot2017/subsystem/DriveTrainTransmissionSubsystem.java
+++ src/com/team2502/robot2017/subsystem/DriveTrainTransmissionSubsystem.java
@@ -5,8 +5,9 @@ import com.team2502.robot2017.Robot;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-//import edu.wpi.first.wpilibj.DoubleSolenoid;
-
+/**
+ * Contains all the code for the transmission and some of the math relating to Automatic Shifting
+ */
 public class DriveTrainTransmissionSubsystem extends Subsystem
 {
     private static Solenoid switcher;
@@ -49,4 +50,29 @@ public class DriveTrainTransmissionSubsystem extends Subsystem
             switcher.set(this.highGear = highGear);
         }
     }
+
+    /**
+     *
+     * @param fps The number in feet per second to convert to RPM given wheels of 4 inches in diameter
+     * @return The equivalent RPM
+     */
+    public double fpsToRPM(double fps)
+    {
+        return ((fps) * 60 * 12) / (4*Math.PI);
+    }
+
+
+    /**
+     * @param x A number
+     * @return The sign of the number
+     */
+    public double sign(double x) { return Math.abs(x)/x; }
+
+    /**
+     *
+     * @param x A number
+     * @param y Another number
+     * @return If the numbers have the same sign
+     */
+    public boolean signsame(double x, double y) { return sign(x) == sign(y); }
 }
