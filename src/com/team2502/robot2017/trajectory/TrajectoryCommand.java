362b4ec5402a47f3e3cb86f917a8a749baa52203
Ritik Mishra <ritik.mishra314@gmail.com>
2017-09-01 23:59:46 -0500

    Starting to work on trajectories :)

---
+++ src/com/team2502/robot2017/trajectory/TrajectoryCommand.java
@@ -0,0 +1,15 @@
+package com.team2502.robot2017.trajectory;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Created by ritik on 8/31/17.
+ */
+public class TrajectoryCommand extends Command
+{
+    @Override
+    protected boolean isFinished()
+    {
+        return false;
+    }
+}
