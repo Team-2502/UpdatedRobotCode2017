bfefc8e5658e88aa74434b100e74452c052e0f46
Ritik Mishra <ritik.mishra314@gmail.com>
2017-09-02 17:40:39 -0500

    File reader updated to use modern java.io.file

--- src/com/team2502/robot2017/trajectory/AutoPaths.java
+++ src/com/team2502/robot2017/trajectory/AutoPaths.java
@@ -3,8 +3,8 @@ package com.team2502.robot2017.trajectory;
 import java.util.Hashtable;
 
 import com.team2502.robot2017.trajectory.util.PathFileReader;
-import com.team254.lib.trajectory.Path;
-import com.team254.lib.trajectory.io.TextFileDeserializer;
+import com.team2502.robot2017.trajectory.Trajectory;
+import com.team2502.robot2017.trajectory.util.TextFileReader;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
@@ -15,47 +15,46 @@ import edu.wpi.first.wpilibj.Timer;
  */
 public class AutoPaths
 {
-	// Make sure these match up!
-	public static final int WALL_LANE_ID = 2;
-	public final static String[] kPathNames = {"InsideLanePathFar",
-			"CenterLanePathFar",
-			"WallLanePath",
-			"InsideLanePathClose",
-			"StraightAheadPath",
-	};
-	public final static String[] kPathDescriptions = {"Inside, Far",
-			"Middle Lane",
-			"Wall Lane",
-			"Inside, Close",
-			"Straight ahead",
-	};
-	static Hashtable paths_ = new Hashtable();
+    // Make sure these match up!
+    public static final int WALL_LANE_ID = 2;
+    public final static String[] kPathNames = {"InsideLanePathFar",
+            "CenterLanePathFar",
+            "WallLanePath",
+            "InsideLanePathClose",
+            "StraightAheadPath",
+    };
+    public final static String[] kPathDescriptions = {"Inside, Far",
+            "Middle Lane",
+            "Wall Lane",
+            "Inside, Close",
+            "Straight ahead",
+    };
+    static Hashtable paths_ = new Hashtable();
 
-	public static void loadPaths()
-	{
-		Timer t = new Timer();
-		t.start();
-		PathFileReader deserializer = new PathFileReader();
-		for (int i = 0; i < kPathNames.length; ++i)
-		{
+    public static void loadPaths()
+    {
+        Timer t = new Timer();
+        t.start();
+        PathFileReader deserializer = new PathFileReader();
+        for(int i = 0; i < kPathNames.length; ++i)
+        {
 
-			TextFileReader reader = new TextFileReader("file://" + kPathNames[i] +
-					".txt");
+			TextFileReader reader = new TextFileReader("file://" + kPathNames[i] + ".txt");
 
 			Path path = deserializer.deserialize(reader.readWholeFile());
 			paths_.put(kPathNames[i], path);
-		}
-		System.out.println("Parsing paths took: " + t.get());
-	}
+        }
+        System.out.println("Parsing paths took: " + t.get());
+    }
 
-	public static Path get(String name)
-	{
-		return (Path) paths_.get(name);
-	}
+    public static Path get(String name)
+    {
+        return (Path) paths_.get(name);
+    }
 
-	public static Path getByIndex(int index)
-	{
-		return (Path) paths_.get(kPathNames[index]);
-	}
+    public static Path getByIndex(int index)
+    {
+        return (Path) paths_.get(kPathNames[index]);
+    }
 }
 
